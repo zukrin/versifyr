4e0f77137f8be951ccd1c9351c8e78d7719d204f
Stefano Zuccaro <stefano.zuccaro@posteitaliane.it>
2023-05-23 17:55:54 +0200

    fix: update version

--- internal/versifyr/version.go
+++ internal/versifyr/version.go
@@ -1,11 +1,12 @@
 package versifyr
 
 // $versifyr:template=const Version = "{{ .version }}"$
-const Version = "v0.0.1"
+const Version = "v0.0.2"
 
 // $versifyr:template=const Sample = "{{ .sample }}"$
 const Sample = "something"
 
 // $versifyr:template=const Compiled = "{{ .actualtimestamp }}"$
-const Compiled = "2023-05-23 10:29:22"
+const Compiled = "2023-05-23 17:55:38"
+
 
