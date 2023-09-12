83cd76699cc0b1ee4438af74910b337d0f5be4d3
Stefano Zuccaro <stefano.zuccaro@posteitaliane.it>
2023-09-12 14:14:55 +0200

    fix: version

--- internal/versifyr/version.go
+++ internal/versifyr/version.go
@@ -1,13 +1,14 @@
 package versifyr
 
 // $versifyr:template=const Version = "{{ .version }}"$
-const Version = "v0.0.8"
+const Version = "v0.0.9"
 
 // $versifyr:template=const Sample = "{{ .sample }}"$
 const Sample = "<no value>"
 
 // $versifyr:template=const Compiled = "{{ .actualtimestamp }}"$
-const Compiled = "2023-09-12 13:59:25"
+const Compiled = "2023-09-12 14:14:24"
+
 
 
 
