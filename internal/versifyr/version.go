86ca7066d81f410fb34c5ea418dbc9efbc9173bb
Stefano Zuccaro <stefano.zuccaro@posteitaliane.it>
2026-03-31 11:36:40 +0200

    chore: update Taskfile versioning to use latest git tag
    - Fetch current version from git tags instead of source file
    - Correctly increment semantic version for automatic advancement
    - Update project version to v0.0.17

--- internal/versifyr/version.go
+++ internal/versifyr/version.go
@@ -1,13 +1,13 @@
 package versifyr
 
 // $versifyr:template=const Version = "{{ .version }}"$
-const Version = "v0.0.16"
+const Version = "v0.0.17"
 
 // $versifyr:template=const Sample = "{{ .sample }}"$
 const Sample = "<no value>"
 
 // $versifyr:template=const ActualTimestamp = "{{ .version | replace "." "_" }}"$
-const ActualTimestamp = "v0_0_16"
+const ActualTimestamp = "v0_0_17"
 
 // $versifyr:template=const Compiled = "{{ .actualtimestamp }}"$
-const Compiled = "2026-03-22 15:12:28"
+const Compiled = "2026-03-31 11:36:33"
