7fa65edd23d1a32269ec2c08373ad33d87371c31
Stefano Zuccaro <stefano.zuccaro@posteitaliane.it>
2026-03-31 15:12:50 +0200

    chore: advance project version to v0.1.2

--- internal/versifyr/version.go
+++ internal/versifyr/version.go
@@ -1,13 +1,13 @@
 package versifyr
 
 // $versifyr:template=const Version = "{{ .version }}"$
-const Version = "v0.1.1"
+const Version = "v0.1.2"
 
 // $versifyr:template=const Sample = "{{ .sample }}"$
 const Sample = "<no value>"
 
 // $versifyr:template=const ActualTimestamp = "{{ .version | replace "." "_" }}"$
-const ActualTimestamp = "v0_1_1"
+const ActualTimestamp = "v0_1_2"
 
 // $versifyr:template=const Compiled = "{{ .actualtimestamp }}"$
-const Compiled = "2026-03-31 14:57:44"
+const Compiled = "2026-03-31 15:12:50"
