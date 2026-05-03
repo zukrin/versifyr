1109a2e3fd3d0caf199572e00386333da8c94013
Stefano Zuccaro <stefano.zuccaro@posteitaliane.it>
2026-05-03 16:00:59 +0200

    chore: advance version to v0.0.19
    - Updated version and sample quote per release standards.

--- internal/versifyr/version.go
+++ internal/versifyr/version.go
@@ -1,13 +1,13 @@
 package versifyr
 
 // $versifyr:template=const Version = "{{ .version }}"$
-const Version = "v0.0.18"
+const Version = "v0.0.19"
 
 // $versifyr:template=const Sample = "{{ .sample }}"$
-const Sample = "The light that burns twice as bright burns half as long."
+const Sample = "Fear is the mind-killer."
 
 // $versifyr:template=const ActualTimestamp = "{{ .version | replace "." "_" }}"$
-const ActualTimestamp = "v0_0_18"
+const ActualTimestamp = "v0_0_19"
 
 // $versifyr:template=const Compiled = "{{ .actualtimestamp }}"$
-const Compiled = "2026-05-01 19:02:43"
+const Compiled = "2026-05-03 16:00:59"
