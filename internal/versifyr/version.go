05e86424b21ae54bf382197e3f4dc1f9c35ab3a3
Stefano Zuccaro <stefano.zuccaro@posteitaliane.it>
2026-05-12 15:27:48 +0200

    chore: bump version to v0.0.20

--- internal/versifyr/version.go
+++ internal/versifyr/version.go
@@ -1,13 +1,13 @@
 package versifyr
 
 // $versifyr:template=const Version = "{{ .version }}"$
-const Version = "v0.0.19"
+const Version = "v0.0.20"
 
 // $versifyr:template=const Sample = "{{ .sample }}"$
-const Sample = "Fear is the mind-killer."
+const Sample = "The stars are fire, and the void between them is ice"
 
 // $versifyr:template=const ActualTimestamp = "{{ .version | replace "." "_" }}"$
-const ActualTimestamp = "v0_0_19"
+const ActualTimestamp = "v0_0_20"
 
 // $versifyr:template=const Compiled = "{{ .actualtimestamp }}"$
-const Compiled = "2026-05-03 16:00:59"
+const Compiled = "2026-05-12 15:27:36"
